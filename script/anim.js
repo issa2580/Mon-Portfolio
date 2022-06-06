186f760a68b1282046d78b91a1327df90b04a6e0
issa2580 <issadiop2580@gmail.com>
2022-06-06 17:00:20 +0000

    mon premier commit

---
+++ script/anim.js
@@ -0,0 +1,14 @@
+const titreSpan = document.querySelectorAll('h1 span');
+const btn = document.querySelectorAll('.btn-menu');
+const logo = document.querySelectorAll('.logo');
+const medias = document.querySelectorAll('.buble');
+const line1 = document.querySelectorAll('.line1'); 
+const line2 = document.querySelectorAll('.line2'); 
+const line3 = document.querySelectorAll('.line3'); 
+const line4 = document.querySelectorAll('.line4'); 
+const line5 = document.querySelectorAll('.line5'); 
+const line6 = document.querySelectorAll('.line6'); 
+
+window.addEventListener('load', () => {
+    const TimeLine = gsap+-
+})
